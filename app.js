b7230373e6d7be455afe2b874809055cebfb68cb
juanncu <antony556_04@outlook.com>
2024-09-18 10:21:28 -0600

    routes and controllers of auth added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import express from "express";
 export const app = express();
 import { User } from "./models/User.js";
+import authRoutes from "./routes/auth.js";
+dotenv.config();
 
 //sincronizacion de modelos y tablas en la DB
 try {
@@ -11,8 +12,13 @@ try {
 } catch (error) {
   console.log("Error al sincronizar los modelos: ", error);
 }
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//RUTAS DE LA APP
+//rutas de autenticacion
+app.use("/api/auth", authRoutes);
---
+++ controllers/authcontroller.js
@@ -0,0 +1,10 @@
+import bcrypt from "bcrypt";
+import { User } from "../models/User.js";
+
+export const register = (req, res) => {
+  //se
+};
+
+export const login = (req, res) => {
+  //dsf
+};
