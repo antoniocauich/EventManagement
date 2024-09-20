34d58ae79a55092c26ce6b6b8c0be47d4888bb73
juanncu <antony556_04@outlook.com>
2024-09-19 21:35:34 -0600

    login user added

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,10 +1,12 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { User } from "../models/User.js";
 
+//Register
 export const register = async (req, res) => {
-  const { name, email, password, role } = req.body;
-
   try {
+    const { name, email, password, role } = req.body;
+
     const existUser = await User.findOne({ where: { email } }); //buscar existencia de usuario
 
     if (existUser) {
@@ -28,6 +30,35 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  //dsf
+//Login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existUser = await User.findOne({ where: { email } }); //buscar existencia de usuario
+
+    if (!existUser) {
+      return res
+        .status(400)
+        .json({ message: "Correo o contraseña incorrecta" });
+    }
+
+    const comparePassword = await bcrypt.compare(password, existUser.password);
+
+    if (!comparePassword) {
+      return res
+        .status(400)
+        .json({ message: "Correo o contraseña incorrecta" });
+    }
+
+    const token = jwt.sign(
+      { id: existUser.id, name: existUser.name, email: existUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ message: "Login correcto. Bienvenido!!", token });
+  } catch (error) {
+    res.status(500).json({ message: "Error en el servidor" });
+  }
 };
